8cba56ed69f19823990e8edfeb8c4b9a352de309
Jordi-R66 <66806189+Jordi-R66@users.noreply.github.com>
2026-03-28 16:50:56 +0100

    Déclaration de nouveaux types à partir de structs pour représenter les clés El-Gamal

--- src/elgamal/elgamal.h
+++ src/elgamal/elgamal.h
@@ -1,2 +1,27 @@
 #pragma once
 
+#include "../common_headers/includes.h"
+#include "../common_headers/utils.h"
+
+#pragma pack()
+
+typedef struct {
+	CustomInteger x;
+} EGPrivateKey;
+
+typedef struct {
+	CustomInteger p, q, a, h;
+} EGPublicKey;
+
+typedef struct {
+	EGPrivateKey priv;
+	EGPublicKey pub;
+} EGKeyPair;
+
+#define EG_PUBLIC_KEY_SIZE sizeof(EGPublicKey);
+#define EG_PRIVATE_KEY_SIZE sizeof(EGPrivateKey);
+#define EG_KEYPAIR_SIZE sizeof(EGKeyPair)
+
+#pragma pack(1)
+
+EGKeyPair generateEGKeyPair(SizeT bits);
